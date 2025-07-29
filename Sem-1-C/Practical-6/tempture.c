146f0b019ee015897f1966c84943b8fab4d6f7be
Rupala Rut <rutrupala.ce@gmail.com>
2025-07-29 10:19:08 +0530

    Supllymentry problem 1 solved

--- Sem-1-C/Practical-6/tempture.c
+++ Sem-1-C/Practical-6/tempture.c
@@ -3,19 +3,17 @@
 void main(){
 
     float InputTempt, OutputTempt;
-    int option;
     char unit;
 
-    printf("Welcome to Tempt Convertor Choose option to convert :\n ");
+    printf("Welcome to Tempt Convertor :\n ");
     printf("Enter Temp in with unit: ");
     scanf("%f%c",&InputTempt,&unit);
-    printf("\nNow Choose Convert method \n 1 for C->F \n 2 for F->c \n Choose : ");
-    scanf("%d",&option);
-    if (option == 1)
+
+    if (unit == 'c' || unit == 'C')
     {
         OutputTempt = (InputTempt*9/5) + 32;
         printf("Result : %.2f F",OutputTempt);
-    }else if(option == 2)
+    }else if(unit == 'F' || unit == 'f' )
     {
         OutputTempt = (InputTempt-32)*5/9;
         printf("Result : %.2f C",OutputTempt);
@@ -23,4 +21,5 @@ void main(){
     {
      printf("Invalid Input Type.");
     }
+    printf("\nThanks for Using it. :)");
 }
