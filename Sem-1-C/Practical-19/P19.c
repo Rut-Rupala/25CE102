06b1f7c16dd939d4e3f957a6f4e59c28c626e71b
Ghost <rutrupala.ce@gmail.com>
2025-09-23 10:40:59 +0530

    P19 second time

--- Sem-1-C/Practical-19/P19.c
+++ Sem-1-C/Practical-19/P19.c
@@ -17,20 +17,29 @@ int main() {
     }
     printf("\n");
 
-    // (ii) Hourglass / X-shape numbers
-    n = 9;  // width
-    printf("(ii) X-shape Numbers\n");
-    for (i = 1; i <= n; i++) {
-        for (j = 1; j <= n; j++) {
-            if (j == i || j == (n - i + 1))
-                printf("%d ", j);
-            else
+    n =5;
+    printf("(ii)\n");
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n*2 -1; j++)
+        {
+            if (i == 1)
+            {
+                printf(" %d",j);
+            }
+            else if (j == i || j == n*2 -i)
+            {
+                printf(" %d",j);
+            }
+            else{
                 printf("  ");
+            }
+            
         }
-        printf("\n");
+        
+        printf("\n");        
     }
     printf("\n");
-
     // (iii) Diamond with numbers
     n = 5;  // size
     printf("(iii) Diamond Numbers\n");
@@ -55,26 +64,46 @@ int main() {
     printf("\n");
 
     // (iv) Alphabet Pyramid
-    n = 4;  // rows
+    n = 5;  // rows
     printf("(iv) Alphabet Pyramid\n");
-    for (i = 1; i <= n; i++) {
-        for (j = i; j < n; j++)
-            printf("  ");
-        for (j = 1; j <= i; j++)
-            printf("%c", 'A' + j - 1);
-        for (j = i - 1; j >= 1; j--)
-            printf("%c", 'A' + j - 1);
-        printf("\n");
+    for (int i = 1; i <= n; i++)
+    {   
+        char alpha = 'A';
+        for (int s = n; s >= i; s--)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%c",alpha);
+            alpha++;
+        }
+        for (int j = 2; j <= i; j++)
+        {
+            alpha = 'A'+i-j;
+            printf("%c",alpha);
+        }
+        printf("\n");    
     }
-    for (i = n - 1; i >= 1; i--) {
-        for (j = i; j < n; j++)
-            printf("  ");
-        for (j = 1; j <= i; j++)
-            printf("%c", 'A' + j - 1);
-        for (j = i - 1; j >= 1; j--)
-            printf("%c", 'A' + j - 1);
-        printf("\n");
+    for (int i = 1; i <= n; i++)
+    {
+        char alpha = 'A';
+        for (int s = 0; s<=i; s++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j <= n- i; j++)
+        {
+            printf("%c",alpha++);
+        }
+        alpha--;
+        for (int j = 1; j <= n-i -1; j++)
+        {
+            printf("%c",--alpha);
+        }
+        
+        printf("\n");  
     }
-
+    
     return 0;
 }
