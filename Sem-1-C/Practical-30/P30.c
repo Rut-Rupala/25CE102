ec619d32e1e82daa04dc59431cba727df1ff9711
Rupala Rut <rutrupala.ce@gmail.com>
2025-11-04 10:54:53 +0530

    P-30

---
+++ Sem-1-C/Practical-30/P30.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+void main(){
+    int size;
+    printf("Enter Size of arr : ");
+    scanf("%d",&size);
+    if (size <= 0)
+    {
+        printf("Invild size!");
+         return;
+    }
+    int arr[size];
+    int *p;
+    p =arr;
+    printf("Enter Elements of arr : \n");
+    for (int i = 0; i < size; i++)
+    {
+        scanf("%d",p+i);
+    }
+    
+    
+    
+    for (int i = 0; i < size; i++)
+    {
+        for(int j = i; j < size; j++){
+            if (p[i]>p[j])
+            {
+                /* code */
+                
+                int temp;
+                temp = p[j];
+                p[j] = p[i];
+                p[i] = temp;
+            }
+        }
+    }
+
+    printf("Output : ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ",p[i]);
+    }
+    
+    
+}
